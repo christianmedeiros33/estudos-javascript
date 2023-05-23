a7ef1682faf0fc8baaeeebe22416f03213793280
Christian medeiros <christianmedeiros022@gmail.com>
2023-05-22 22:49:19 -0300

    .

--- 8-undefined-null/index.js
+++ 8-undefined-null/index.js
@@ -1,3 +1,16 @@
 //null e undefined, Object
 
+// hoisting - içamento funções e variáveis inicializadas no topo do código
+console.log(sobrenome);
 
+var nome = null;
+var sobrenome = "medeiros";
+
+console.log(nome);
+console.log(sobrenome);
+
+nome = "christian";
+
+console.log(nome);
+
+var numero = 5;
